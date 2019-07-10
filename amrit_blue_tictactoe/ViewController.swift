8adfea81f750dff2585d75e2f6c234693f25b820
Amrit Pal Singh <amritpalsingha4@gmail.com>
2019-07-10 17:06:22 -0400

    add dynamic image allocation

--- amrit_blue_tictactoe/ViewController.swift
+++ amrit_blue_tictactoe/ViewController.swift
@@ -13,14 +13,15 @@ class ViewController: UIViewController {
     @IBOutlet var collection: [UIView]!
     @IBOutlet var imageCollection: [UIImageView]!
     
-    var currentPlayer = TicTacToe.zero
+    var currentPlayer = TicTacToe.cross
+    
+    var game = TicTacToe()
     
     @IBAction func Button(_ sender: UIButton) {
         // sender.titleEdgeInsets = UIEdgeInsets(top: 10,left: 10,bottom: 10,right: 10)
     }
     
     func addImageTapGesture(imageView : UIImageView) {
-        print("Somehting")
         let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.imageTapped(tapGestureRecognizer:)))
         imageView.isUserInteractionEnabled = true
         imageView.addGestureRecognizer(tapGestureRecognizer)
@@ -28,18 +29,34 @@ class ViewController: UIViewController {
     
     @objc func imageTapped(tapGestureRecognizer: UITapGestureRecognizer) {
         let tappedImage = tapGestureRecognizer.view as! UIImageView
-        tappedImage.image = UIImage(named: TicTacToe.cross)
-        // Your action
-        print("Tapped Image")
+        var index = imageCollection.firstIndex(of: tappedImage)!
+        
+//        ONLY CARRY ON IF THE TAPPED IMAGE IS AN EMPTY ONE
+        if (game.sprites[index] == nil) {
+            print(index, game.sprites[index])
+            
+            tappedImage.image = UIImage(named: currentPlayer)
+            
+            game.playTurn(index: index, turn: currentPlayer)
+            print(game.sprites, "---", index)
+            
+            //        SWITCH TURNS
+            currentPlayer = switchTurns(currentPlayer: currentPlayer)
+        }
+       
+    }
+    
+    func switchTurns(currentPlayer: String) -> String {
+        return currentPlayer == TicTacToe.cross ? TicTacToe.zero : TicTacToe.cross
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        for part in collection! {
-            // addPanGesture(view: part)
-            print("COLORS - " , part.backgroundColor!)
-        }
+//        for part in collection! {
+//            // addPanGesture(view: part)
+//            print("COLORS - " , part.backgroundColor!)
+//        }
         
         for part in imageCollection! {
             addImageTapGesture(imageView: part)

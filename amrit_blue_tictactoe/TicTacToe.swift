df246b6828ff332e1569aff81179d3325398cc67
Amrit <amritpalsingha4@gmail.com>
2019-07-10 15:35:43 -0400

    add files for checkpoint

---
+++ amrit_blue_tictactoe/TicTacToe.swift
@@ -0,0 +1,19 @@
+//
+//  TicTacToe.swift
+//  amrit_blue_tictactoe
+//
+//  Created by Amrit on 2019-06-08.
+//  Copyright © 2019 Singh Singh. All rights reserved.
+//
+
+import Foundation
+
+class TicTacToe {
+    var values : [String]?
+    
+    static let zero = "Zero"
+    static let cross = "Cross"
+    static let transparent = "Transparent"
+    
+    
+}
--- amrit_blue_tictactoe/ViewController.swift
+++ amrit_blue_tictactoe/ViewController.swift
@@ -10,46 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var collection: [UIView]!
+    @IBOutlet var imageCollection: [UIImageView]!
     
-    @IBOutlet var allSquares: [UIView]!
-    @IBOutlet weak var orangeRectangle: UIView!
+    var currentPlayer = TicTacToe.zero
     
     @IBAction func Button(_ sender: UIButton) {
         // sender.titleEdgeInsets = UIEdgeInsets(top: 10,left: 10,bottom: 10,right: 10)
     }
-
-    func addPanGesture(view : UIView) {
-        let pan = UIPanGestureRecognizer(target: self
-            , action: #selector(ViewController.handlePan(_:)))
-        view.addGestureRecognizer(pan)
+    
+    func addImageTapGesture(imageView : UIImageView) {
+        print("Somehting")
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.imageTapped(tapGestureRecognizer:)))
+        imageView.isUserInteractionEnabled = true
+        imageView.addGestureRecognizer(tapGestureRecognizer)
     }
     
-    @objc func handlePan(_ gestureRecognizer: UIPanGestureRecognizer) {
-//        let fileView = sender.view
-//        let translation = sender.translation(in: view)
-//        print(sender.state)
-
-            guard gestureRecognizer.view != nil else { return }
-            
-            if gestureRecognizer.state == .ended {      // Move the view down and to the right when tapped.
-                let animator = UIViewPropertyAnimator(duration: 0.2, curve: .easeInOut, animations: {
-                    gestureRecognizer.view!.center.x += 100
-                    gestureRecognizer.view!.center.y += 100
-                })
-                animator.startAnimation()
-            }
-        
+    @objc func imageTapped(tapGestureRecognizer: UITapGestureRecognizer) {
+        let tappedImage = tapGestureRecognizer.view as! UIImageView
+        tappedImage.image = UIImage(named: TicTacToe.cross)
+        // Your action
+        print("Tapped Image")
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        addPanGesture(view: orangeRectangle!)
         
-        for square in allSquares
-            print("SA")
+        for part in collection! {
+            // addPanGesture(view: part)
+            print("COLORS - " , part.backgroundColor!)
+        }
+        
+        for part in imageCollection! {
+            addImageTapGesture(imageView: part)
+            part.image = UIImage(named: TicTacToe.transparent)
+            print("COLORS - ")
+        }
         
     }
+    
+    
+    
 
 
 }

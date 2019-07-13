81c5ad4376154506107af48f3fc38caeb276cad4
Amrit <amritpalsingha4@gmail.com>
2019-07-12 23:16:25 -0400

    fixed everything, history works

--- amrit_blue_tictactoe/ViewController.swift
+++ amrit_blue_tictactoe/ViewController.swift
@@ -9,90 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet var collection: [UIView]!
-    @IBOutlet var imageCollection: [UIImageView]!
-    
-    var currentPlayer = TicTacToe.cross
-    
-    var game = TicTacToe()
-    var stopFurtherInteractions = false
-    
-    @IBAction func Button(_ sender: UIButton) {
-        // sender.titleEdgeInsets = UIEdgeInsets(top: 10,left: 10,bottom: 10,right: 10)
-    }
-    
-    func addImageTapGesture(imageView : UIImageView) {
-        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.imageTapped(tapGestureRecognizer:)))
-        imageView.isUserInteractionEnabled = true
-        imageView.addGestureRecognizer(tapGestureRecognizer)
-    }
-    
-    @objc func imageTapped(tapGestureRecognizer: UITapGestureRecognizer) {
-        let tappedImage = tapGestureRecognizer.view as! UIImageView
-        let index = imageCollection.firstIndex(of: tappedImage)!
-        
-//        ONLY CARRY ON IF THE TAPPED IMAGE IS AN EMPTY ONE
-        if (game.sprites[index] == "" && !stopFurtherInteractions) {
-            print(index, game.sprites[index])
-            
-            tappedImage.image = UIImage(named: currentPlayer)
-            
-            game.playTurn(index: index, turn: currentPlayer)
-            print(game.sprites, "---", index)
-            
-            //        SWITCH TURNS
-            currentPlayer = switchTurns(currentPlayer: currentPlayer)
-            let winner = game.checkForWinner()
-            print(winner)
-            if (winner != "") {
-                showMessage(message: "\(winner) won")
-                stopFurtherInteractions = true
-            } else {
-                showCurrentPlayerTurnMessage(message: nil, font: nil)
-            }
-            
-        }
-        
-    }
-    
-    func switchTurns(currentPlayer: String) -> String {
-        return currentPlayer == TicTacToe.cross ? TicTacToe.zero : TicTacToe.cross
-    }
-    
-    func showMessage(message:String) {
-        let toastLabel = UILabel(frame: CGRect(x: self.view.frame.size.width/2 - 75, y: self.view.frame.size.height-100, width: 150, height: 35))
-        toastLabel.backgroundColor = UIColor.black
-        toastLabel.textColor = UIColor.white
-        toastLabel.font = UIFont(name: "IranSansMobile", size: 19)
-        toastLabel.textAlignment = .center;
-        toastLabel.text = message
-        toastLabel.alpha = 1.0
-        toastLabel.layer.cornerRadius = 10;
-        toastLabel.clipsToBounds  =  true
-        toastLabel.adjustsFontSizeToFitWidth = true
-        self.view.addSubview(toastLabel)
-    }
-    
-    func showCurrentPlayerTurnMessage(message : String?, font: UIFont?) {
-        
-        let message = "   Player \(currentPlayer == TicTacToe.cross ? "1" : "2")'s (\(currentPlayer)) turn  .";
-        showMessage(message: message)
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        for part in imageCollection! {
-            addImageTapGesture(imageView: part)
-            part.image = UIImage(named: TicTacToe.transparent)
-            print("COLORS - ")
-        }
-        
     }
-    
-    
-    
-
 
 }
 
